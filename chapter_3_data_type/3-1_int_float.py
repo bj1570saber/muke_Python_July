1d0668b4e5095f08b2f034cd11cbdd265fa5df5e
bj1570Saber <bj1570saber@gmail.com>
2019-11-11 23:49:48 -0800

    int_float

--- chapter_3_data_type/3-1_int_float.py
+++ chapter_3_data_type/3-1_int_float.py
@@ -1,5 +1,13 @@
-print(type(1))
+print('-1:%s ' %(type(-1)))
 n = 2
-print(type(n))
+print('n:%s ' %(type(n)))
 m = 3.14
-print(type(m))
+print('m:%s ' %(type(m)))
+print('1.111111111111111111111111111111111111:%s ' %(type(1.111111111111111111111111111111111111)))
+print('1+1 = :%s' %(type(1+1)))
+print('1+1.0 = :%s' %(type(1+1.0)))
+print('1*1 = :%s' %(type(1*1)))
+print('1*1.0 = :%s' %(type(1*1.0)))
+print('1/1 = :%s' %(type(1/1)))
+print('1//1 = %d:%s' %(1//1, type(1//1))) #another type of divide for int result
+print('3//2 = %d:%s' %(3//2, type(3//2))) #another type of divide for int result
