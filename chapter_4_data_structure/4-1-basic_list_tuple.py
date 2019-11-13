b16bcbdb48859a396e58376dabecc5b8a3d03e76
bj1570Saber <bj1570saber@gmail.com>
2019-11-12 22:06:10 -0800

    1-1-basic_list_tuple.py updated

--- chapter_4_data_structure/4-1-basic_list_tuple.py
+++ chapter_4_data_structure/4-1-basic_list_tuple.py
@@ -1,23 +1,25 @@
 #List example:
 students= ['Jerry','Tom','Adm',10, True]
-print('students[0]:%s' %(students[0]))
-print('students[-1]:%s' %(students[-1]))
-print('students[-2]:%s' %(students[-2]))
-students.append('Jack')#append
-print('students[-2]:%s' %(students[-2]))
 print(students)
-students.insert(1, 'Bob')#insert
-print(students)
+print('students[0]:%s' %(students[0])) # Jerry
+print('students[-1]:%s' %(students[-1]))#True
+print('students[-2]:%s' %(students[-2]))#10
+students.append('Jack')#append()
+print('students[-2]:%s' %(students[-2]))#True
+print(students)# ['Jerry', 'Tom', 'Adm', 10, True, 'Jack']
+students.insert(1, 'Bob')#insert()
+print(students)#['Jerry', 'Bob', 'Tom', 'Adm', 10, True, 'Jack']
 students.pop()#pop
-print(students)
+print(students)#['Jerry', 'Bob', 'Tom', 'Adm', 10, True]
 students.pop(0)
-print('length: %d' %len(students))
-print(students)
+print(students)#['Bob', 'Tom', 'Adm', 10, True]
+print('length: %d' %len(students))#5
 students.insert(2,['asp', 'php'])
-print(students)
-print(students[2][1])
+print(students)#['Bob', 'Tom', ['asp', 'php'], 'Adm', 10, True]
+print(students[2][1])#php
 
 #Tuple Example: can not change after create.
+print('\nTuple example:')
 tuple_1 = (1,2,'string',True)
 print(tuple_1) # (1, 2, 'string', True)
 # tuple_1[2] = 10 # can not assign
@@ -25,9 +27,9 @@ print(tuple_1[2]) # string
 print(tuple_1[-1]) # True
 
 tuple_2 = (1) # a variable
-print(tuple_2)# output 1
+print(tuple_2)# output:1
 tuple_2 = (1,)# one element tuple
-print(tuple_2)# output: (1,)
+print(tuple_2)# output:(1,)
 tuple_3 = () # empty tuple
-print(tuple_3)
+print(tuple_3)# output:()
 
