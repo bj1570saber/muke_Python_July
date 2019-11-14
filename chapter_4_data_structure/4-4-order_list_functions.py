57203afe3c42f3b4a338f8506c331c281ec48481
bj1570Saber <bj1570saber@gmail.com>
2019-11-14 12:50:02 -0800

    4-4 done

--- chapter_4_data_structure/4-4-order_list_functions.py
+++ chapter_4_data_structure/4-4-order_list_functions.py
@@ -48,5 +48,26 @@ print(str_1[-1:-8:-3])#doo
 #https://blog.csdn.net/xiaofeiyu321/article/details/82941765
 
 #in
-print("'in' function:")
+print("\n'in' function:")
+#list_1= [1,2,3,4,5,6]
+print(2 in list_1)#True
+print(3 not in list_1)#False
 
+print("\n'len()' function:")
+print(len(list_1))#6
+
+str_2 = 'HelloWorld'
+print("\n'max()' function:")
+print(max(list_1))#6
+print(max(tuple_1))#d
+print(max(str_2))#r ASC II 
+
+print("\n'min()' function:")
+print(min(list_1))#1
+print(min(tuple_1))#a
+print(min(str_2))#H
+
+print("\nord(), return ASC II order:")
+print(ord('r'))#114
+print(ord('H'))#72
+print(ord(' '))#32
