c54c5082f54824c450abdebc43ba80a926c7b9cd
bj1570Saber <bj1570saber@gmail.com>
2019-11-14 17:31:06 -0800

    5-2-variable_type.py

--- chapter_3_data_type/3-7_string_print.py
+++ chapter_3_data_type/3-7_string_print.py
@@ -3,22 +3,22 @@ print('Let\'s go.')
 print('''
 Hello
 World
-I
-am
-Jerry
 ''')
 
 print("""
-Hello
-World
 I
 am
 Jerry
 """)
 
 print('hello \
-world.')# Above line can not comment. 
+world.')# output: hello world. Above line can not comment or any space. 
 # " \ + returnKey " allow the code to be continue in next line
-# output: hello world.
+
+print(1+2\
+    +3)#6  #returnKey, no space after it.
+
+#print raw string: with - r
+print(r'I like \n')#I like \n
 
 
