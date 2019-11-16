98239ee48059efa221aa95d895280a8b910b3d17
bj1570Saber <bj1570saber@gmail.com>
2019-11-15 21:19:18 -0800

    7-3

--- cha_6_7_flow_control_N_loop/7-3_rang.py
+++ cha_6_7_flow_control_N_loop/7-3_rang.py
@@ -1,2 +1,5 @@
-for x in range(0,10):
-    print(x)
+for x in range(0,11,2):# start point, end point(not include), interval,
+    print(x, end = ' | ')
+print()
+for x in range(11,0, -2):# start point, end point(not include), vector,
+    print(x, end = ' | ')
