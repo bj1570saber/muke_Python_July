a79b25e3cf6c8a4c8b345ca6dc39c385a1771272
bj1570Saber <bj1570saber@gmail.com>
2019-11-15 19:26:08 -0800

    7-1_while loop

---
+++ cha_6_7_flow_control_N_loop/7-1_while_loop.py
@@ -0,0 +1,10 @@
+count = 1
+while count < 3:
+    print(count)
+    count += 1
+count = 1
+while count < 6:
+    print(count)
+    count += 1
+else:# Just like if-else. else will be invoke when condition == False:
+    print('END')
