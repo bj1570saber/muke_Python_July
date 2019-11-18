8ddd0688e992118a1569c267f900b9e9092309c8
bj1570Saber <bj1570saber@gmail.com>
2019-11-17 21:25:12 -0800

    namespace-done

--- cha_6_7_flow_control_N_loop/7_6_namesapce.py
+++ cha_6_7_flow_control_N_loop/7_6_namesapce.py
@@ -1,11 +1,12 @@
 import c7
-import new.c1
+import new.c1 as c1
 #import cha_6_7_flow_control_N_loop # error
 
 # namespace: the module's path.
-# Python package must has __init__.py files. It can be empty or not. 
+# Python package must includes a __init__.py files. It can be empty or not. 
 # import __init__.py : import the package name.
 
-#print(cha_6_7_flow_control_N_loop.b)
+#print(cha_6_7_flow_control_N_loop.b) # error ??????
 print(c7.c)
-print(new.c1.c)
+#print(new.c1.c) # longer typing version
+print(c1.c)# shorter module path name use keyword 'as'
