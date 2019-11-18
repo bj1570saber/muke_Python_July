3095d27d6c18f9b12ca3128b699b254d86f772cc
bj1570Saber <bj1570saber@gmail.com>
2019-11-17 21:53:13 -0800

    7-8 from import

--- cha_6_7_flow_control_N_loop/7_8_from_import.py
+++ cha_6_7_flow_control_N_loop/7_8_from_import.py
@@ -1,6 +1,15 @@
 from c7 import c,c_2 # import variable
 from new import c1 # import module
 from new.c2 import *
+from new.c3 import c3_1, c3_2,\
+    c3_3 # If import too many variable. use \ start a new line or ()
+'''
+from new.c3 import (c3_1, c3_2,
+    c3_3)
+'''
+## () also use in expression to start a new line
+print(1+2+
+3)
 
 print(c)
 print(c_2)
@@ -8,3 +17,5 @@ print(c1.c)
 print(c2_1)
 print(c2_2)
 print(c2_3)
+
+print(c3_3)
