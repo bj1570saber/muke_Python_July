1c19624f8d74a79e245d13c5a7853fd4905c94e3
bj1570Saber <bj1570saber@gmail.com>
2019-11-19 23:55:24 -0800

    7_13_name

--- cha_6_7_flow_control_N_loop/7_13_dir_name.py
+++ cha_6_7_flow_control_N_loop/7_13_dir_name.py
@@ -1,3 +1,8 @@
 import sys,c7
 print(dir(sys)) # dir() retrieves variables list from parameter module.
 print("\nc7.py: "+ str(dir(c7)))
+
+# How to use __name__
+if __name__ =='__main__':
+    print('This is an app.') # This line will not print, if another .py file import this module.
+print("This is a module.")
