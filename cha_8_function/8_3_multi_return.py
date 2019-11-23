fcee7a343221216028914c7b346dd272e117a807
bj1570Saber <bj1570saber@gmail.com>
2019-11-22 23:14:52 -0800

    8_3_multi_return.py

---
+++ cha_8_function/8_3_multi_return.py
@@ -0,0 +1,8 @@
+def damage(skill1, skill2):
+    damage1 = skill1 * 3
+    damage2 = skill2 * 2 + 10
+    return damage1, damage2
+
+damages = damage(3,6)
+print(damages[0], damages[1])
+print(type(damages))
