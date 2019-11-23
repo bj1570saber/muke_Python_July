5cd56b5bc6ab82d914465344bbab20b5be510d2f
bj1570Saber <bj1570saber@gmail.com>
2019-11-22 23:58:38 -0800

    8_5

--- cha_8_function/8_5_parameter_type.py
+++ cha_8_function/8_5_parameter_type.py
@@ -1,6 +1,18 @@
 def double_skill_1_combine(skill_1,skill_2):
     return skill_1 * 2 + skill_2
 
-# 关键字参数：arguemnts' order doesnt mmatter.
-c = double_skill_1_combine(skill_2 = 2, skill_1 = 1 )
-print(c)
+# 1.必须参数：
+print(double_skill_1_combine(1,2))
+
+# 2.关键字参数：arguemnts' order doesnt mmatter.
+d = double_skill_1_combine(skill_2 = 2, skill_1 = 1 )
+print(d)
+
+# 3. 默认参数
+def print_student_files(name,gender,age,city):
+    print("My name is " + name)
+    print("I am " + str(age) + " years old.")
+    print("I'm "+ gender)
+    print("I live in " + city)
+
+print_student_files("Jerry", "male", 28, "SD")
