5d8ba9662a9ceaca60871eed0e41fe05dfcf663b
bj1570Saber <bj1570saber@gmail.com>
2019-11-24 23:56:30 -0800

    8_7_dynamic_param.py

--- cha_8_function/8_7_dynamic_param.py
+++ cha_8_function/8_7_dynamic_param.py
@@ -21,5 +21,26 @@ print('~'*20)
 # a fix:
 demo_1(*a)#('a', 'b', 'c') #<class 'tuple'>
 demo_1(*b)#('a', 'b', 'c') #<class 'tuple'>
+print('~'*20)
 
+# different type parameter's order：必须参数，默认参数，可变参数
+def demo_2(param1, param2=2, *param):
+    print(param1)
+    print(param2)
+    print(param)
+    
+demo_2('a',1,2,3,4,'param')
+# a
+# 1
+# (2, 3, 4)
+print('~'*20)
 
+def demo_3(param1, *param, param2=2):
+    print(param1)
+    print(param2)
+    print(param)
+
+demo_3('a',1,2,3,4,'param')
+# a
+# 2
+# (1, 2, 3, 4, 'param')
