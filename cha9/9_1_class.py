fc5aaef574280e16fc5c09a45f7d63562f2be3ef
Bingjie Zhai <saber@Bingjies-MacBook-Pro.local>
2019-11-26 23:29:56 -0800

    9_1_class.py

---
+++ cha9/9_1_class.py
@@ -0,0 +1,17 @@
+# 面向对象
+# 有意义的面向对象
+# 类 = 面向对象
+# 类最基本的作用：封装
+
+class Student():
+    name = ''
+    age = 0
+
+    def print_file(self):
+        print('name: ' + self.name)
+        print('age: '+ str(self.age))
+
+#test:
+# student = Student()
+# student.print_file()
+
