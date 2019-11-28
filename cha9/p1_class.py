c68be92264ce190cc6462c3aff08b17c97a09c81
bj1570saber <bj1570saber@gmail.com>
2019-11-28 01:05:54 -0800

    class

--- cha9/p1_class.py
+++ cha9/p1_class.py
@@ -5,18 +5,16 @@
 # class encapsulates data(fields) and actions(functions) of same kind of objects.
 
 class Student():
-    # fields
-    name = ''
-    age = 0
+    # class fields
+    sum = 0
+    name='null'
+    age = 1
     # functions
-    def __init__(self): #constructor
-        print('student')
+    def __init__(self, name, age): #constructor
+        self.name = name # object field 'self.name'
+        self.age = age # object field 'self.age'
+        print('constructor of '+ self.name)
         # return None # only return None (default or explicit).
-
-    # def __init__(self, name, age):
-    #     self.__init__()
-    #     self.name = name
-    #     self.age = age
 
     def introduce(self):
         print('name: ' + self.name)
@@ -26,10 +24,11 @@ class Student():
         print("do_homework")
 
 #test:
-student1 = Student()
+student1 = Student("Jerry", 2)
+print(student1.name)
 student1.introduce()
 student1.do_homework()
 
-a = student1.__init__()#return default None to a
-print(type(a))# <class 'NoneType'>
-print(a)# None
+# a = student1.__init__("Tom", 2)#return default None to a
+# print(type(a))# <class 'NoneType'>
+# print(a)# None
