b831b027bb2bd6bb19376b644579e337b4d0f9a5
bj1570saber <bj1570saber@gmail.com>
2019-11-29 00:08:21 -0800

    9_7

---
+++ cha9/9_7_variable.py
@@ -0,0 +1,25 @@
+class Student():
+    name = 'None'
+    age = 0
+    
+    def __init__(self, name, age):# 'self' can be any words such as 'this'
+        self.name = name # object field 'self.name'
+        self.age = age # object field 'self.age'
+        print('constructor of '+ self.name)
+        print(name)
+        print(age)
+        # return None # only return None (default or explicit).
+
+student1 = Student("Jerry", 20)
+print(student1.__dict__)#{'name': 'Jerry', 'age': 20}
+# search variable path:
+# object.variable -> class.variable -> ancestor class variable
+
+print(Student.__dict__)# print class variables
+'''
+{'__module__': '__main__', 'name': '', 'age': 0, 
+'__init__': <function Student.__init__ at 0x1080ef700>, 
+'__dict__': <attribute '__dict__' of 'Student' objects>, 
+'__weakref__': <attribute '__weakref__' of 'Student' objects>, 
+'__doc__': None}
+'''
