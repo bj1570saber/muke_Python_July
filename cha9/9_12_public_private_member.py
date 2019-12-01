f9c6e641cc464fc0b2095eeaca1737197bffae1d
bj1570saber <bj1570saber@gmail.com>
2019-11-30 23:55:57 -0800

    9_12

--- cha9/9_12_public_private_member.py
+++ cha9/9_12_public_private_member.py
@@ -10,6 +10,8 @@ class Student():
         print('constructor of '+ self.name)
 
     def marking(self, score_p):
+        if score_p < 0:
+            return "score can not be negtive."
         self.score = score_p
         print(self.name + '\'s score: ' + str(self.score))
 
@@ -22,44 +24,14 @@ class Student():
 
 student1 = Student("Jerry", 20)
 student1.do_homework()#外部调用
-student1.marking(59)
+result =student1.marking(59)
+if result is not None:
+    print(result)
+result =student1.marking(-1)
+if result is not None:
+    print(result)
 # output:
-
-
-
-    # @classmethod
-    # def plus_sum(cls):
-    #     cls.sum += 1 
-    #     print("Total number of students:" + str(cls.sum))
-    #     print("cls.sum: " + str(cls.sum))# can access Class field.
-    #     print("cls.name: " + cls.name)#this print Studnet.name; 
-        # can not access object field.
-
-    #staticmethod can be replace by classmethod. 
-    # It's similar to object function and class function,
-    # but lack of parameter(cls, self)
-
-    # If a method not relate to class and object, 
-    # can as a static method, lack of Object Oriented Programming.
-
-    # @staticmethod
-    # def get_sum():# no self or cls parameter
-    #     print('Static method get_sum: ' + str(Student.sum))# access class field
-    #     #print(self.name)# can not access object field.
-
-# student1 = Student("Jerry", 20)
-# student1.plus_sum()# invoke class method by object
-# Student.plus_sum()# invoke class  method by Class
-# student1.get_sum()# invoke static method by object
-# Student.get_sum()# invoke static method by Class
-
-# # output:
 # constructor of Jerry
-# Total number of students:1
-# cls.sum: 1
-# cls.name: None
-# Total number of students:2
-# cls.sum: 2
-# cls.name: None
-# Static method get_sum: 2
-# Static method get_sum: 2
+# Jerry is doing homework.
+# Jerry's score: 59
+# score can not be negtive.
