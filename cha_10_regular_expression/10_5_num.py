bead1f156d2bd41a798646c6b7babeee7eb3a1b2
bj1570saber <bj1570saber@gmail.com>
2019-12-03 20:32:23 -0800

    10_5

--- cha_10_regular_expression/10_5_num.py
+++ cha_10_regular_expression/10_5_num.py
@@ -1,11 +1,17 @@
 #数量词
 import re
 a = 'python1111java678php'
-
-r = re.findall('[a-z]{3,6}', a) 
+# Python default greedy filter(as long as possible).
+r = re.findall('[a-z]{3,7}', a) 
 # num will allowd to filter length between 3 - 6 words.
-print(r)
+print(r)#['python', 'java', 'php']
 
 r = re.findall('[a-z0-3]{3,6}', a) 
 # num will allowd to filter length between 3 - 6 words.
-print(r)
+print(r)#['python', '1111ja', 'php']
+print('~' * 20)
+
+# Force None greedy filter(as short as possible)
+r = re.findall('[a-z]{3,6}?', a) # '?' marker to force None greedy.
+# num will allowd to filter length between 3 - 6 words.
+print(r)#['pyt', 'hon', 'jav', 'php']
