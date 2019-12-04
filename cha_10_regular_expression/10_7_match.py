60ee50649d6a450271a4d52fe51040a71b949ed0
bj1570saber <bj1570saber@gmail.com>
2019-12-03 21:44:23 -0800

    10_7

---
+++ cha_10_regular_expression/10_7_match.py
@@ -0,0 +1,28 @@
+
+import re
+a = 'pyth_pytho0python1pythonn2pythonnn'
+
+# * match previous char 'n' 0 or many times
+r = re.findall('python*', a)
+print(r)# ['pytho', 'python', 'pythonn', 'pythonnn']
+print('~' * 20)
+
+# + match previous char 'n' 1 or many times
+r = re.findall('python+', a)
+print(r)# ['python', 'pythonn', 'pythonnn']
+print('~' * 20)
+
+# ? match previous char 'n' 1 or 0 time
+r = re.findall('python?', a)
+print(r)#['pytho', 'python', 'python', 'python']
+print('~' * 20)
+
+# ? match previous char 'n' 1-3 times OK
+r = re.findall('python{1,3}', a) # accept 1-3 times 'n'.
+print(r)#['python', 'pythonn', 'pythonnn']
+print('~' * 20)
+
+### ?: force None Greedy
+# ? match previous char 'n' 1 time only 
+r = re.findall('python{1,3}?', a) # as less 'n' as possible.
+print(r)#['python', 'python', 'python']
