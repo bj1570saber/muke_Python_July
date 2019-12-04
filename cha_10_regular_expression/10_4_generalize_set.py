a4b6ee18354cd10c7a2ff321fb62d2226a511c8e
bj1570saber <bj1570saber@gmail.com>
2019-12-03 19:54:39 -0800

    10_4

--- cha_10_regular_expression/10_4_generalize_set.py
+++ cha_10_regular_expression/10_4_generalize_set.py
@@ -4,18 +4,23 @@
 # \s blank character \S: not blank character
 
 import re
-a = 'python 11\t11\njava&678\rphp'
+a = 'python 11\t11\njava&678\rphp__'
 w = re.findall('\w', a)#[A-Za-z0-9]
 print(w)
+print('~'*20)
 
-w_2 = re.findall('[A-Za-z0-9]', a)
+w_2 = re.findall('[A-Za-z0-9_]', a)
 print(w_2)
+print('~'*40)
 
 not_w = re.findall('\W', a)#[A-Za-z0-9]
 print(not_w)
+print('~'*30)
 
 blank = re.findall('\s', a)#[]
 print(blank)
+print('~'*20)
 
 not_blank = re.findall('\S', a)#[]
 print(not_blank)
+print('~'*40)
