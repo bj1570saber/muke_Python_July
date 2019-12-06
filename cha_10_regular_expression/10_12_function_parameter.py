14d37cf83f9e9a1d494d57d82d9c0ee1586fb2fe
bj1570saber <bj1570saber@gmail.com>
2019-12-05 20:08:29 -0800

    revise

--- cha_10_regular_expression/10_12_function_parameter.py
+++ cha_10_regular_expression/10_12_function_parameter.py
@@ -11,3 +11,22 @@ def convert(value):
     
 r = re.sub('\d',convert, s)# every digit will be apple convert()
 print(r)#A9C0900D99
+print('~'* 20)
+
+s = 'A8C3721D086'
+def convert_multi_digits(value):
+    matched = value.group()
+    #print(matched)
+    # print(matched.find('0')==0)
+    if matched.find('0') == 0:
+        if int(matched) == 0:
+            return '0'
+        else: 
+            return '0'+'2'
+    elif int(matched) > 9:
+        return '2'
+    else:
+        return '1'
+
+r = re.sub('\d+',convert_multi_digits, s)# every digit will be apple convert()
+print(r)#A1C2D2
