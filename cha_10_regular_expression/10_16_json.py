2b721c3710c1407e32293d2f8445337769854e77
bj1570saber <bj1570saber@gmail.com>
2019-12-06 23:23:47 -0800

    10_16

--- cha_10_regular_expression/10_16_json.py
+++ cha_10_regular_expression/10_16_json.py
@@ -6,6 +6,16 @@
 import json
 json_str = '{"name":"qiyue", "age":18}' # be careful with ' and  "
 student = json.loads(json_str)
-print(type(student))
-print(student)
+print(type(student))#<class 'dict'>
+print(student)#{'name': 'qiyue', 'age': 18}  Note: ' '
+print(student['name'])#qiyue
+print('~' * 20)
 
+json_str = '[{"name":"qiyue", "age":18,"flag": false},{"name":"Tom", "age":20}]'
+student = json.loads(json_str)
+print(type(student))#<class 'list'>
+print(type(student[1]))#<class 'dict'>       # Note: this is 'False', in JSON is 'false'
+print(student)#[{'name': 'qiyue', 'age': 18, 'flag': False}, 
+              # {'name': 'Tom', 'age': 20}] 
+print('~' * 20)
+# deserialization：The process of transfer string to specific programming data structure.
