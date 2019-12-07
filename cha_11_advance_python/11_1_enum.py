d2bfc514d378ee6ab385058958773880ede5e684
bj1570saber <bj1570saber@gmail.com>
2019-12-07 13:13:12 -0800

    11_1

--- cha_11_advance_python/11_1_enum.py
+++ cha_11_advance_python/11_1_enum.py
@@ -1,10 +1,20 @@
 from enum import Enum
-# Improve readability.
-class VIP(Enum): 
-    YELLOW = 1
-    GREEN = 2
-    BLACK = 3
-    RED = 4
+
+# Why use enum? :
+class VIP(Enum): # Improve readability.
+    YELLOW = 1 # different enum has different value.
+    GREEN = 2 # variable name call 'key', it's unique.
+    BLACK = 3 # key can not reassign new value.
+    RED = 4 # Immutable during use, after define.
 
 print(VIP.YELLOW) # VIP.YELLOW  
-print(type(VIP.YELLOW)) #<enum 'VIP'>
+print(type(VIP.YELLOW)) # <enum 'VIP'>
+print('~')
+print(VIP.YELLOW.value) # 1
+print(VIP.GREEN.name) # GREEN
+print('~')
+print(type(VIP.GREEN.name)) # <class 'str'>
+print(VIP['GREEN'])# VIP.GREEN
+print('~')
+for v in VIP:
+    print(v)
