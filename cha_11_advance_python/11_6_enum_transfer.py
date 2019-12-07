2d2d0712b629fdb4cfe75780882befe10dc32b3d
bj1570saber <bj1570saber@gmail.com>
2019-12-07 14:15:07 -0800

    11-6-enum done

--- cha_11_advance_python/11_6_enum_transfer.py
+++ cha_11_advance_python/11_6_enum_transfer.py
@@ -1,4 +1,4 @@
-from enum import Enum
+from enum import Enum, IntEnum, unique
 
 class VIP(Enum): 
     YELLOW = 1 
@@ -9,3 +9,15 @@ class VIP(Enum):
 a = 1 # suppost I got 1 as value from database.
 # looking for the key of enum:
 print(VIP(a))# VIP.YELLOW
+print('~')
+@unique# decorater：all value must be unique.
+class VIPI(IntEnum): # IntEnum parent class force child class's enum value are integer.
+    YELLOW = 1 
+   #YELLOW_ALIAS = 'str' # ValueError: invalid literal for int() with base 10: 'str'
+    BLACK = 3 
+    #RED = 3
+    #ValueError: duplicate values found in <enum 'VIPI'>: RED -> BLACK
+
+for v in VIPI:
+    print(v)
+
