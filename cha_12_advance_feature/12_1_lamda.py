df80536a3cdcb8f6014398a5e3e778223b0aa74b
bj1570saber <bj1570saber@gmail.com>
2019-12-08 11:23:51 -0800

    12_3

---
+++ cha_12_advance_feature/12_1_lamda.py
@@ -0,0 +1,16 @@
+# lambda expression
+
+def add(x, y):
+    return x+y
+
+print(add(1,2))
+
+f = lambda x,y: x+y
+print(f(1,2))
+print('~')
+
+# ternary expression 三元表达式
+x = 2
+y = 1
+r = x if x > y else y
+print(r)
