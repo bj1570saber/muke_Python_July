d1edb6dcf35dadaba03a9a753af810b650eb2e6a
bj1570saber <bj1570saber@gmail.com>
2019-12-08 12:08:03 -0800

    12_6

---
+++ cha_12_advance_feature/12_6_filter.py
@@ -0,0 +1,14 @@
+
+list_x =[1,0,3,4,0,6,0,8]
+r = filter(lambda x: True if x != 0 else False, list_x)# filter out 0
+# lambda expression must return T or F, T will stay, F will remove.
+print(list(r))#[1, 3, 4, 6, 8]
+
+r = filter(lambda x: x, list_x)# filter out 0
+# 0: False other int are True
+print(list(r))#[1, 3, 4, 6, 8]
+
+# filter out lower case:
+list_l = ['A','a','b','B','C','c']
+l = filter(lambda x: True if x.isupper() else False, list_l)
+print(list(l))#['A', 'B', 'C']
