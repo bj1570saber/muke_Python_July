f3d772434b7dc58e35cc2adc80b29542fef778c4
bj1570saber <bj1570saber@gmail.com>
2019-12-09 19:48:20 -0800

    group

--- cha_10_regular_expression/10_9_group.py
+++ cha_10_regular_expression/10_9_group.py
@@ -1,8 +1,8 @@
 import re
-a = 'PythonPythonPythonJSPythonPython'
+a = 'PythonPythonPythonJSPythonPythonPython'
 #
 r = re.findall('(Python){3}', a)#'PythonPythonPython'
-print(r)#['Python']
+print(r)#['Python', 'Python']
 
 #
 r = re.findall('(Python){4}', a)
@@ -12,3 +12,11 @@ print(r)#[]
 r = re.findall('(Python){3}(JS)(Python)', a)
 print(r)#[('Python', 'JS', 'Python')]
 
+#
+a = 'PythonPythonPython JS PythonPythonPython'
+r = re.findall('(Python){3}(JS)(Python)', a)
+print(r)#[] did not filter out space.
+
+#
+r = re.findall('(Python){3}\s(JS)\s(Python)', a)
+print(r)#[('Python', 'JS', 'Python')]  \s - filter out space.
