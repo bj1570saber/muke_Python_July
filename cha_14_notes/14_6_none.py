0a40f676de87c25321b85f042af313676f113f0b
bj1570saber <bj1570saber@gmail.com>
2019-12-10 15:44:33 -0800

    14_6

--- cha_14_notes/14_6_none.py
+++ cha_14_notes/14_6_none.py
@@ -1,5 +1,4 @@
 # None is not: '', [], 0, False
-
 a = ''
 b = False
 c = []
@@ -10,3 +9,26 @@ print(c == None)#False
 print(a is None)#False
 print(None)#None 
 print(type(None))#<class 'NoneType'>
+
+# If Else Control:
+def fun():
+    return None
+
+a = fun()
+if not a: # better
+    print('S')
+else:
+    print('F')
+
+if a is None: # DO NOT USE
+    print('S')
+else:
+    print('F')
+
+list_a = [None, '', [], False, 0, (), {None}]# {None} -> True
+i = 0
+while i< len(list_a) and not list_a[i]:
+    i += 1
+    print(a)
+print('End')
+    
