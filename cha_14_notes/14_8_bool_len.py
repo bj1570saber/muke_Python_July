74fcf4ea4d1324be3b9fd07347ba7e5ab96c6cfd
bj1570saber <bj1570saber@gmail.com>
2019-12-10 17:13:22 -0800

    14_8

--- cha_14_notes/14_8_bool_len.py
+++ cha_14_notes/14_8_bool_len.py
@@ -7,14 +7,32 @@ class Test_4():
 
 #print(len(Test_4)) # TypeError: object of type 'type' has no len() 
 
-print('Test_5:~~~')
+print('Test_5:__bool__() priority')
 class Test_5():
     def __bool__(self): # this function has priority to return T/F
+        print('bool called.')
         return False 
+
     def __len__(self):
+        print('len called.')
         return True
 test_5 = Test_5()
 if test_5:
     print('True')
 else:
     print('False')
+# bool called.
+# False
+
+
+print('Test_6: __bool__() return type')
+class Test_6():
+    def __bool__(self): # this function has priority to return T/F
+        return 0 
+    def __len__(self):
+        return True
+test_6 = Test_6()
+# if test_6:#TypeError: __bool__ should return bool, returned int
+#     print('True')
+# else:
+#     print('False')
