2d8689ff826bb698d828a428343d88e950bdf955
bj1570saber <bj1570saber@gmail.com>
2019-12-16 23:59:34 -0800

    int float

--- cha_3_data_type/3_1_int_float.py
+++ cha_3_data_type/3_1_int_float.py
@@ -1,13 +1,18 @@
-print('-1:', type(-1))#-1: <class 'int'>
+print('-1:', type(-1))# -1: <class 'int'>
 n = 2
-print('n: ', type(n))#n:  <class 'int'>
+print('n: ', type(n))# n:  <class 'int'>
 m = 3.14
-print('m:%s ' %(type(m)))
-print('1.111:%s ' %(type(1.111)))
-print('1+1 = %d :%s' %(1+1,type(1+1)))
-print('1+1.0 = %.2f :%s' %(1+1.0,type(1+1.0)))
-print('1*1 = %d :%s' %(1*1,type(1*1)))
-print('1*1.0 = %f :%s' %(1*1.0,type(1*1.0)))
+print('m: ', type(m))# m:  <class 'float'>
+print('1.111: ',type(1.111))#1.111:  <class 'float'>
+print('~')
+print('1+1 =', 1+1, type(1+1))# 1+1 = 2 <class 'int'>
+print('1+1.0 =', 1+1.0, type(1+1.0))# 1+1.0 = 2.0 <class 'float'>
+print('~~~')
+print('2-1 =', 2-1, type(2-1))# 2-1 = 1 <class 'int'>
+print('2-1.0 =', 2-1.0, type(2-1.0))# 2-1.0 = 1.0 <class 'float'>
+print('~~~~~')
+print('1*1 =', 1*1, type(1*1))# 1*1 = 1 <class 'int'>
+print('1*1.0 =', 1*1.0, type(1*1.0))# 1*1.0 = 1.0 <class 'float'>
 print('~'*20)
 # / operator output:
 print('/ operator output: ')
@@ -36,3 +41,8 @@ print('~')
 print('4//2.0 = ', 4//2.0) # 4//2.0 =  2.0
 print('4//2.0 : ', type(4//2.0))# 4//2.0 :  <class 'float'>
 # int -> int type result; float -> float type result;
+print('~'*20)
+
+# modulo %
+print('10 % 3 = ', 10 % 3)# 10 % 3 =  1
+print(type(10 % 3))# <class 'int'>
