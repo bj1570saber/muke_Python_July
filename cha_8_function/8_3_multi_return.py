d9f1a3ed0fd7e3ea9ccb95d5b82ec09ef1632ab0
bj1570saber <bj1570saber@gmail.com>
2019-12-18 13:45:28 -0800

    function revise

--- cha_8_function/8_3_multi_return.py
+++ cha_8_function/8_3_multi_return.py
@@ -7,8 +7,8 @@ damages = damage(3,6)#<class 'tuple'>
 print(type(damages))
 print(damages[0], damages[1])
 # 9 22
-# 9 22
 
 # A better way to print:
 skill_1_damage, skill_2_damage = damage(3,6)#序列解包 sequence unpacking
 print(skill_1_damage, skill_2_damage)# more meaningful than index print.
+# 9 22
---
+++ test.py
@@ -0,0 +1,6 @@
+def my_abs(x):
+    return x, -x
+
+a,b = my_abs(1)
+print(a)
+print(b)
